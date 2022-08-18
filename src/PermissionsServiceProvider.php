28a56331596989fad9bdcf52f61f456146c76b53
RomanStruk <romanuch4@gmail.com>
2022-08-18 21:43:53 +0300

    update composer

---
+++ src/PermissionsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace RomanStruk\LaravelPermissionUi;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+
+    public function boot(): void
+    {
+
+    }
+}
